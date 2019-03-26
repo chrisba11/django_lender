60d8f3afab44ee050106523857b006ed922310de
Chris Ball <chrisball7@gmail.com>
2019-03-26 16:50:59 -0700

    added more to Book model

--- lender_app/models.py
+++ lender_app/models.py
@@ -14,15 +14,11 @@ class Book(models.Model):
     author = models.CharField(max_length=48),
     year = models.IntegerField(max_length=4)
     status = models.CharField(choices=STATES, default='in', max_length=24)
-    date_added = models.DateField
-    last_borrowed =
-
+    date_added = models.DateField(auto_now_add=True)
+    last_borrowed = models.DateField(auto_now=True)
 
     def __repr__(self):
         return '<Book: {} | Status: {}>'.format(self.title, self.status)
 
-
     def __str__(self):
         return 'Book: {} | Status: {}'.format(self.title, self.status)
-
-
