d3123c7973e8f03bc8c3b99e67ab21028f659ff6
Chris Ball <chrisball7@gmail.com>
2019-03-27 06:26:34 -0700

    ran into problems with migrations and wasn't able to solve them before heading to meetup

--- lender_app/models.py
+++ lender_app/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.utils.timezone import now
 
 
 class Book(models.Model):
@@ -10,11 +11,11 @@ class Book(models.Model):
         ('out', 'Checked-Out'),
     ]
 
-    title = models.CharField(max_length=48),
-    author = models.CharField(max_length=48),
-    year = models.IntegerField(max_length=4)
-    status = models.CharField(choices=STATES, default='in', max_length=24)
-    date_added = models.DateField(auto_now_add=True)
+    title = models.CharField(max_length=48)
+    author = models.CharField(max_length=48)
+    year = models.IntegerField(default=0000)
+    status = models.CharField(choices=STATES, default='in', max_length=48)
+    date_added = models.DateField(default=now)
     last_borrowed = models.DateField(auto_now=True)
 
     def __repr__(self):
