2c44dd78f175ecee045c71dfc68fb49b8af052c7
Chris Ball <chrisball7@gmail.com>
2019-03-27 17:40:08 -0700

    7 tests passing

--- lender_app/tests.py
+++ lender_app/tests.py
@@ -1,3 +1,57 @@
-from django.test import TestCase
+from django.test import TestCase, RequestFactory
+from django.http import Http404
+from .models import Book
+from .views import book_detail_view, book_list_view
 
-# Create your tests here.
+
+class TestBooksModels(TestCase):
+    def setUp(self):
+        Book.objects.create(title='title one', author='author one', year='2001')
+        Book.objects.create(title='title two', author='author two', year='2001')
+        Book.objects.create(title='title three', author='author three', year='2001')
+
+    def test_book_titles(self):
+        one = Book.objects.get(title='title one')
+        two = Book.objects.get(title='title two')
+        self.assertEqual(one.title, 'title one')
+        self.assertEqual(two.title, 'title two')
+
+    def test_book_authors(self):
+        one = Book.objects.get(title='title one')
+        two = Book.objects.get(title='title two')
+        self.assertEqual(one.author, 'author one')
+        self.assertEqual(two.author, 'author two')
+
+
+class TestNotesViews(TestCase):
+    def setUp(self):
+        self.request = RequestFactory()
+
+        Book.objects.create(title='title one', author='author one', year='2001')
+        Book.objects.create(title='title two', author='author two', year='2001')
+        Book.objects.create(title='title three', author='author three', year='2001')
+
+    def test_book_detail_view_context(self):
+        request = self.request.get('')
+        response = book_detail_view(request, f'{ Book.objects.get(title="title one").id }')
+        self.assertIn(b'author one', response.content)
+
+    def test_book_list_view_context(self):
+        request = self.request.get('')
+        response = book_list_view(request)
+        self.assertIn(b'title two', response.content)
+
+    def test_book_detail_view_status_code_success(self):
+        request = self.request.get('')
+        response = book_detail_view(request, f'{ Book.objects.get(title="title one").id }')
+        self.assertEqual(response.status_code, 200)
+
+    def test_book_list_view_status_code_success(self):
+        request = self.request.get('')
+        response = book_list_view(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_book_detail_view_status_code_failure(self):
+        request = self.request.get('')
+        with self.assertRaises(Http404):
+            book_detail_view(request, '0')
