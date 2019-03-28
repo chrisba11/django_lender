466a9d413299351082c725f2402d02b6e2438479
Chris Ball <chrisball7@gmail.com>
2019-03-28 13:36:21 -0700

    added Django Registration Settings

--- django_lender/settings.py
+++ django_lender/settings.py
@@ -128,3 +128,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# Django Registration Settings
+ACCOUNT_ACTIVATION_DAYS = 1
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+if DEBUG:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
