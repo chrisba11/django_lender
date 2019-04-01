2a9ffc680d3d1e871c044a7569de3832424de581
Chris Ball <chrisball7@gmail.com>
2019-03-31 17:41:49 -0700

    get_date_string function written

--- lender_app/templatetags/template_extras.py
+++ lender_app/templatetags/template_extras.py
@@ -1,3 +1,18 @@
 from django.utils import timezone
 from django import template
 
+
+register = template.Library()
+
+
+@register.filter
+def get_date_string(value):
+    now_aware = timezone.now()
+    delta = value - now_aware
+
+    if delta.days == 0:
+        return 'Today'
+    elif delta.days == 1:
+        return 'Yesterday'
+    elif delta.days > 1:
+        return '{} days ago'.format(delta.days)
